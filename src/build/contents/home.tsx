c17aac22e23eaa1b11656936d43b220759efa947
Chris <scharl.chrissi2006@gmail.com>
2025-12-02 23:27:41 +0100

    some test contents

---
+++ src/build/contents/home.tsx
@@ -0,0 +1,11 @@
+export const NavInformation = () => {
+    return (
+        <p>NavInformation</p>
+    );
+}
+
+export const GeneralInformation = () => {
+    return (
+        <p>General Information</p>
+    );
+}
