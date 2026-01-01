bfa41516b6f66dacd0c86fdf5d90e79231080c03
Chris <scharl.chrissi2006@gmail.com>
2026-01-02 00:29:32 +0100

    Revert "Merge pull request #1 from Eins-Chris/copilot/add-electron-support"
    This reverts commit 252aa26087b96a47324b2bebff2611abe3302cd6, reversing
    changes made to b82e7b4c0c87978f6d7beb98d74040aefaa4fa4e.

--- v2/electron/config.js
+++
@@ -1,4 +0,0 @@
-// Shared configuration for Electron and Vite
-module.exports = {
-  DEV_SERVER_PORT: 5173
-};
--- v2/electron/preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge } = require('electron');
-
-// Expose protected methods that allow the renderer process to use
-// specific Node.js features without exposing the entire Node.js API
-contextBridge.exposeInMainWorld('electronAPI', {
-  platform: process.platform
-});
