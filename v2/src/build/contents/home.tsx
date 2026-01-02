87866abdde8b99f7ac70001924a56d8e90fd14d0
Chris <scharl.chrissi2006@gmail.com>
2026-01-02 02:42:24 +0100

    later implementation of R.A.I.N.E.R

--- v2/src/build/contents/home.tsx
+++ v2/src/build/contents/home.tsx
@@ -1,5 +1,8 @@
 export const home = () => {
     return (
         <p>Home</p>
+        /* Gedanke: ein iframe, der die Contents von einem localhost:idk anzeigt. Dieses localhost ist 
+        ein Visual vom RainerAssistent, der dir dann sachen Visualisieren kann. Da wir dafür nur einen 
+        Client wollen, läuft dieser seperat zum Ui und wird nur mit einem iframe darauf abgebildet */
     );
 }
