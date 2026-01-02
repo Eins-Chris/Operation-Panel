f311af25977ed9663f558da1c61514a8a9e631b9
Chris <scharl.chrissi2006@gmail.com>
2026-01-03 00:52:47 +0100

    Create user_selection.tsx

---
+++ src/build/user_selection.tsx
@@ -0,0 +1,16 @@
+const UserSelection = () => {
+    /* 7 Fenster mit auswählbaren Usern */
+    /* 7 Fenster mit auswählbaren bestehenden Sessions */
+    /* Ausgegraut, was nicht möglich ist (wenn für user keine Session besteht, Feld leer) */
+    /* durch onClick={() => setUserSelected(true)} wird in die app geschalten */
+
+    console.log()
+
+    return (
+        <>
+            <p>User Selection Buttons</p>    
+        </>
+    );
+}
+
+export default UserSelection;
